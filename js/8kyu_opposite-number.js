ac148bd1f4bef35ef5bd31a353ed4fae84291df3
bitsByB <croak.imply99@deepblue.addymail.com>
2022-05-28 16:44:10 -0400

    cw-opposite-number

---
+++ js/8kyu_opposite-number.js
@@ -0,0 +1,21 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// Parameters: Is it always going to be integers?
+// Return: return it's opposite
+// Examples: if we are given 10, should return -10
+//           if we are given -3.8, should return 3.8
+//           if we are given 0, should return 0
+// Pseudocode:
+// Make a function that takes in an integer or a floating-point number
+// return its opposite
+function opposite(number) {
+  return -number
+}
+// test:
+console.log(opposite(-3.8),3.8)
